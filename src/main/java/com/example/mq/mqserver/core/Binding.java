505665b19be391a41057f98f6e312ad75cfc8615
如风暖阳 <1491788197@qq.com>
2024-02-16 23:31:07 +0800

    corecode

---
+++ src/main/java/com/example/mq/mqserver/core/Binding.java
@@ -0,0 +1,39 @@
+package com.example.mq.mqserver.core;
+/*
+ * 表示队列和交换机之间的关联关系
+ */
+public class Binding {
+    private String exchangeName;
+    private String queueName;
+    // bindingKey：队列上的字符串，用于和 Message 的routingKey 做匹配
+    private String bindingKey;
+
+    // Binding是依附于 Exchange 和Queue 的
+    // 比如，对于持久化来说，如果 Exchange 和Queue 任何一个都没有持久化
+    // 此时针对 Binding 持久化是没有任何意义的
+
+
+    public String getExchangeName() {
+        return exchangeName;
+    }
+
+    public void setExchangeName(String exchangeName) {
+        this.exchangeName = exchangeName;
+    }
+
+    public String getQueueName() {
+        return queueName;
+    }
+
+    public void setQueueName(String queueName) {
+        this.queueName = queueName;
+    }
+
+    public String getBindingKey() {
+        return bindingKey;
+    }
+
+    public void setBindingKey(String bindingKey) {
+        this.bindingKey = bindingKey;
+    }
+}
---
+++ src/main/java/com/example/mq/mqserver/core/Exchange.java
@@ -0,0 +1,56 @@
+package com.example.mq.mqserver.core;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+* 这个类表示一个交换机
+ */
+public class Exchange {
+    // 此处使用 name 来作为交换机的身份标识 （唯一的）
+    private String name;
+    // 交换机类型, DIRECT, FANOUT, TOPIC
+    private ExchangeType type=ExchangeType.DIRECT;
+    // 该交换机是否要持久化存储，true 表示需要持久化；false 表示不必持久化
+    private boolean durable=false;
+    // 如果当前交换机，没人使用了，就会自动被删除
+    // 这个属性暂时先列在这里，后续的代码中并没有真的实现这个自动删除功能（RabbitMQ是有的）
+    private boolean autoDelete=false;
+    // arguments 表示的是创建交换机时指定的一些额外的参数选项，后续代码中并没有真的实现对应的功能, 先列出来. (RabbitMQ 也是有的)
+    // 为了把这个 arguments 存到数据库中, 就需要把 Map 转成 json 格式的字符串.
+    private Map<String,Object> arguments=new HashMap<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ExchangeType getType() {
+        return type;
+    }
+
+    public void setType(ExchangeType type) {
+        this.type = type;
+    }
+
+    public boolean isDurable() {
+        return durable;
+    }
+
+    public void setDurable(boolean durable) {
+        this.durable = durable;
+    }
+
+    public boolean isAutoDelete() {
+        return autoDelete;
+    }
+
+    public void setAutoDelete(boolean autoDelete) {
+        this.autoDelete = autoDelete;
+    }
+
+}
